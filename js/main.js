302aad064ea62f3a1b13671e57c00684b15ca1c1
abdulkarimyulchibekov <abdulkarimyulchibekov@gamil.com>
2022-07-05 17:20:27 +0500

    Add aort

--- js/main.js
+++ js/main.js
@@ -1,59 +1,61 @@
 var elSelect = document.querySelector('.js-select');
 const set = new Set();
-for (film of films) { 
-  set.add(...film.genres);
+for (film of films) {
+	set.add(...film.genres);
 }
-set.delete(undefined)
+set.delete(undefined);
 console.log(set);
 
 for (element of set) {
-  const elOption = document.createElement("option");
-  elOption.textContent = element;
-  elOption.value = element;
-  elSelect.appendChild(elOption);
+	const elOption = document.createElement('option');
+	elOption.textContent = element;
+	elOption.value = element;
+	elSelect.appendChild(elOption);
 }
 
-const elList = document.querySelector(".js-list")
+const elList = document.querySelector('.js-list');
 
-const time = function(a) {
-  return (Math.floor(a / 31536000) + 1970);
-}
+const time = function (a) {
+	return Math.floor(a / 31536000) + 1970;
+};
 
 let domgaChiqarator = (array, node) => {
-  array.forEach((film) => {
-    let elItem = document.createElement("li");
-    elItem.classList.add("item")
-    let elSubheader = document.createElement("h3");
-    elSubheader.classList.add("subheader")
-    let elImg = document.createElement("img");
-    let elText = document.createElement("p");
-    elText.classList.add ("text");
-    let array = film.genres;
-    let elSublist = document.createElement("ul")
-    elSublist.classList.add("sublist");
-    for(gen of array) {
-      const elSubitem = document.createElement("li");
-      elSubitem.textContent = gen;
-      elSublist.appendChild(elSubitem);
-    }
-    let elTime = document.createElement("p");
-    elTime.textContent = `year of production: ${time(film.release_date)}`
-    elTime.classList.add("desc");
-  
-    elText.textContent = film.overview;
-    elImg.src = film.poster;
-    elSubheader.textContent = film.title;
-  
-    elItem.appendChild(elImg);
-    elItem.appendChild(elSubheader);
-    elItem.appendChild(elText);
-    elItem.appendChild(elTime);
-    elItem.appendChild(elSublist);
-    elList.appendChild(elItem);
-  })
-}
+	array.forEach((film) => {
+		let elItem = document.createElement('li');
+		elItem.classList.add('item');
+		let elSubheader = document.createElement('h3');
+		elSubheader.classList.add('subheader');
+		let elImg = document.createElement('img');
+		let elText = document.createElement('p');
+		elText.classList.add('text');
+		let array = film.genres;
+		let elSublist = document.createElement('ul');
+		elSublist.classList.add('sublist');
+		for (gen of array) {
+			const elSubitem = document.createElement('li');
+			elSubitem.textContent = gen;
+			elSublist.appendChild(elSubitem);
+		}
+		let elTime = document.createElement('p');
+		elTime.textContent = `year of production: ${time(film.release_date)}`;
+		elTime.classList.add('desc');
+
+		elText.textContent = film.overview;
+		elImg.src = film.poster;
+		elSubheader.textContent = film.title;
+
+		elItem.appendChild(elImg);
+		elItem.appendChild(elSubheader);
+		elItem.appendChild(elText);
+		elItem.appendChild(elTime);
+		elItem.appendChild(elSublist);
+		elList.appendChild(elItem);
+	});
+};
 
 domgaChiqarator(films, elList);
+
+let result = films;
 
 elSelect.addEventListener('change', function () {
 	let selVal = elSelect.value;
@@ -64,5 +66,36 @@ elSelect.addEventListener('change', function () {
 			result.push(poc);
 		}
 	});
-	domgaChiqarator(result, elList)
+	domgaChiqarator(result, elList);
 });
+
+
+
+let aToZ = document.querySelector('.a_to_z');
+let zToA = document.querySelector('.z_to_a');
+
+aToZ.addEventListener('click', function () {
+	let newArray = result.sort((a, b) => {
+		if (a.title > b.title) return 1;
+		return -1;
+	});
+
+  console.log(newArray);
+  elList.innerHTML = "";
+	domgaChiqarator(newArray, elList);
+});
+
+zToA.addEventListener('click', function () {
+	let newArray = result.sort((a, b) => {
+		if (a.title < b.title) return 1;
+		return -1;
+	});
+
+  console.log(newArray);
+  elList.innerHTML = "";
+	domgaChiqarator(newArray, elList);
+});
+
+let News = document.querySelector(".news");
+let Olds = document.querySelector(".olds");
+
